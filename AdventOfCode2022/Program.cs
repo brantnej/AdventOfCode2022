ce9e654b58054edf65e6f9c80cbbe0fbe1b5c344
brantnej <brantnej@oregonstate.edu>
2022-11-30 21:25:12 -0800

    day 1

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿IEnumerable<string> Lines = File.ReadAllLines("Input.txt");
+
+//Part 1
+int max = 0;
+int runningSum = 0;
+foreach (string line in Lines)
+{
+    if (!string.IsNullOrWhiteSpace(line))
+    {
+        runningSum += int.Parse(line);
+    }
+    else
+    {
+        if (runningSum > max)
+        {
+            max = runningSum;
+        }
+        runningSum = 0;
+    }
+}
+Console.WriteLine(max);
+
+//Part 2
+int[] top3 = { 0, 0, 0 };
+runningSum = 0;
+foreach (string line in Lines)
+{
+    if (!string.IsNullOrWhiteSpace(line))
+    {
+        runningSum += int.Parse(line);
+    }
+    else
+    {
+        if (runningSum > top3.Last())
+        {
+            top3[2]= runningSum;
+        }
+        runningSum = 0;
+        Array.Sort(top3);
+        Array.Reverse(top3);
+    }
+}
+Console.WriteLine(top3.Sum());

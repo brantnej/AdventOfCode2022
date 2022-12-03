e8de39c606efb7a705a03d1f9d886a9af9fc4274
brantnej <brantnej@oregonstate.edu>
2022-12-03 13:23:33 -0800

    Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,40 @@
+﻿IEnumerable<string> Lines = File.ReadAllLines("Input.txt");
+
+//Part 1
+int sum = 0;
+foreach (var line in Lines)
+{
+    var compartment1 = line.Substring(0, line.Length / 2);
+    var compartment2 = line.Substring(line.Length/2);
+    foreach (char item in compartment1.Intersect(compartment2))
+    {
+        if (item <= 'Z')
+        {
+            sum += (int)item - 38;
+        }
+        else
+        {
+            sum += (int)item - 96;
+        }
+    }
+}
+Console.WriteLine(sum);
+
+//Part 2
+sum = 0;
+for (int i = 0; i < Lines.Count(); i+= 3)
+{
+    char item = Lines.ElementAt(i)
+        .Intersect(Lines.ElementAt(i + 1))
+        .Intersect(Lines.ElementAt(i + 2))
+        .First();
+    if (item <= 'Z')
+    {
+        sum += (int)item - 38;
+    }
+    else
+    {
+        sum += (int)item - 96;
+    }
+}
+Console.WriteLine(sum);

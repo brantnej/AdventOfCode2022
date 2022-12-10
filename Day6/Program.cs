85e1207e2f84f2a24fbf8bfc4e65ba4283a21aca
brantnej <brantnej@oregonstate.edu>
2022-12-09 22:35:37 -0800

    Day6

---
+++ Day6/Program.cs
@@ -0,0 +1,22 @@
+﻿List<string> Lines = File.ReadAllLines("Input.txt").ToList<string>();
+
+int findStart(int requiredUniques, string line)
+{
+    for (int i = requiredUniques - 1; i < line.Length; i++)
+    {
+        HashSet<char> chars = new HashSet<char>();
+        for (int j = 0; j < requiredUniques; j++)
+        {
+            chars.Add(line[i - j]);
+        }
+        if (chars.Count() == requiredUniques)
+        {
+            return i + 1;
+        }
+    }
+    return 0;
+}
+
+string line = Lines.First();
+Console.WriteLine(findStart(4, line));
+Console.WriteLine(findStart(14, line));
